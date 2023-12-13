c79eeecb05afdbd4d16b316f78b6e65e9a0f4992
Axt3m <angela.terao@gmail.com>
2023-12-13 11:55:58 +0100

    Initial Commit

---
+++ Ricippies/RicippiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicippiesApp.swift
+//  Ricippies
+//
+//  Created by Angela Terao on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RicippiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
